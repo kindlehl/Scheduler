0e16c0afec2a1ee231c517f458fbab1252ee164b
kindlehl <hunter.d.lannon@gmail.com>
2018-04-04 23:36:02 -0700

    Constructs mainMenu using new constructor

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,9 @@ int main(){
 	initscr();	
 	noecho();
 	keypad(stdscr, TRUE);
-	Menu mainMenu;
-	for(int i = 0; i < 10; i++)	
-		mainMenu.add(MenuItem((i*12947)%10, "First Event of the Program", "This is a testing event"));
+	Menu mainMenu("/home/hunter/.schedule");
+//	for(int i = 0; i < 10; i++)	
+//		mainMenu.add(MenuItem((i*12947)%10, "First Event of the Program", "This is a testing event"));
 	while(mainMenu)
 		mainMenu.update();
 	endwin();
--- src/menuitem.cpp
+++ src/menuitem.cpp
@@ -10,6 +10,34 @@ MenuItem::MenuItem(int priority, std::string description, std::string name)
 	selected = false;	
 }
 
+
+/*FILE STRUCTURE
+ * NAME|DESCRIPTION|PRIORITY
+ *
+ * DESIRED BEHAVIOR
+ * Extracts 1 token per DATA MEMBER, discards remaining tokens
+ */
+MenuItem::MenuItem(std::istream& lineFromFile, char delim){
+	//uses getline to extract tokens
+	std::string token;
+	//extract name	
+	std::getline(lineFromFile, token, delim);
+	m_name = token;
+	//extract description
+	std::getline(lineFromFile, token, delim);
+	m_description = token;
+	//extract priority level
+	std::getline(lineFromFile, token, delim);
+	m_priority = atoi(token.c_str());
+	
+	selected = false;
+	//discard remaining bits of line
+	lineFromFile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');	
+	while(lineFromFile.peek() == ' ' || lineFromFile.peek() == '\n'){
+		lineFromFile.ignore();
+	}
+}
+
 bool MenuItem::active() const{
 	return selected;
 }
