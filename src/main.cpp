e66aebedc1b4e8baaae6b2bc82a3422f69e024c0
kindlehl <hunter.d.lannon@gmail.com>
2018-04-08 23:01:07 -0700

    Disabled blinking cursor

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ int main(){
 	initscr();	
 	noecho();
 	keypad(stdscr, TRUE);
+	curs_set(0);
 	Menu mainMenu("/home/hunter/.schedule");
 //	for(int i = 0; i < 10; i++)	
 //		mainMenu.add(MenuItem((i*12947)%10, "First Event of the Program", "This is a testing event"));
