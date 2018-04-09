f766dd82ab716f77d2be357bb1e5f3cfa7d65aed
kindlehl <hunter.d.lannon@gmail.com>
2018-04-08 22:59:17 -0700

    Field sizing and event viewing added
    added the mechanism that lines up the fields in the events, and allowed
    a hoykey to pressed to allow the user to read an event in more detail.

--- include/menu.h
+++ include/menu.h
@@ -1,6 +1,23 @@
 #ifndef MENU_H
 #define MENU_H
 
+//UPDATE THIS EVERYTIME YOU ADD A SEPARATOR OR ANYTHING THAT TAKES UP A STATIC AMOUNT OF SPACE ON THE MENU. THIS INCLUDES A '|' OR A "  ". THIS NUMBER SHOULD BE THE NUMBER OF SPACES USED IN MENU ENTRIES
+#define USED_SPACE (2+3) //parenthasized to prevent fuckery
+//2 spaces at beginning, 3 pipes between items
+
+
+//the following definitions define how much space the windows take, relative to the window. Ensure that the following definitions add to one, or a runtime assertion will break the program.
+#define NAME_PORT .3f 
+#define DESC_PORT .69f
+#define PRIO_PORT .01f
+
+
+//these macros give the number of cells in the terminal that each piece of the menu should take up when printing. This way ensures that the program scales with window resizes.
+#define NAME_SPACING NAME_PORT*(width-USED_SPACE)
+#define DESC_SPACING DESC_PORT*(width-USED_SPACE)
+
+
+
 #include "menuitem.h"
 #include <iostream>
 #include <string>
@@ -23,15 +40,19 @@ class Menu{
 		void add(MenuItem m);
 		void up();
 		void down();
-		//void select();
+		void view();
+		void remove();	
 		void update();
 		void exit();	
 	private:
+		int width, height;
+		bool printField(std::string field, int spaces, char delimiter = ' ');
 		bool run = true;
 		void display();
 		int selectIndex = 0;
 		std::vector<MenuItem> menu_items;
 		void printMenu();	
+		std::vector<int> itemsToExclude;
 };
 
 
--- src/menu.cpp
+++ src/menu.cpp
@@ -21,7 +21,8 @@ Menu::Menu(std::string path){
 		//add items line-by-line
 		menu_items.push_back(MenuItem(file));
 	}
-
+	
+	getmaxyx(stdscr, height, width);
 }
 
 /* This constructor was replaced by the file-reading constructor
@@ -43,42 +44,97 @@ void Menu::update(){
 	this->display();
 	wmove(stdscr, 0,0);			//move cursor to top, so text gets written over	
 	wrefresh(stdscr);
-	char key = getch();
+	chtype key = getch();
 	if(key == 'j')
 		down();
-	if(key == 'k')
+	else if(key == 'k')
 		up();
-	if(key == 'q')
+	else if(key == 'q')
 		Menu::exit();
+	else if(key == 'v')
+		view();
+	else if(key == 'r')
+		remove();
+}
+
+void Menu::remove(){
+	//exclude this line when re-writing file
+	itemsToExclude.push_back(menu_items[selectIndex].ID);
+	//remove from menu
+	menu_items.erase(menu_items.begin()+selectIndex);
+}
+
+//changes the view to display the detailed description of an item along with other details. Dumps the window into a file, then restores the file after the user does not want to view the event anymore.
+void Menu::view(){
+	//save screen to file and clear window
+	scr_dump(".tempscreen");
+	do{
+		move(0,0);
+		clrtobot();
+
+		printField(menu_items[selectIndex].name(), width);
+		addch('\n');
+		printField(menu_items[selectIndex].description(), width);
+		addch('\n');
+	} while(getch() != 'q');
+	move(0,0);
+	clrtobot();
+	scr_restore(".tempscreen");
 }
 
 void Menu::exit(){
 	run = false;
 }
 
-
-
+//Prints a string up to $spaces number of characters, ending with spaces if there is excess and an elipses "..." when there is not enough spaces
+//
+//delimiter defaults to ' '
+bool Menu::printField(std::string field, int spaces, char delimiter){
+	bool tooBig = false;
+	std::string ending = "..."; //used if field is longer than spaces
+	if(field.length() > spaces){
+		tooBig = true;
+		int beginIndex = spaces-ending.length(); //where to insert the ending
+		field.insert(beginIndex, ending);
+	}else{
+		//fills the field with spaces
+		field.append(spaces-field.length(),delimiter);
+	}
+	addnstr(field.c_str(), spaces);	
+	return tooBig;
+}
 
 void Menu::printMenu(){
+	//sorts menu so that higher priority items show up first
 	std::sort(menu_items.begin(), menu_items.end());
 	//prints basic menu
 	for(auto item = menu_items.begin(); item != menu_items.end(); item++){	
-		if(item-menu_items.begin() == selectIndex)
+		if(item-menu_items.begin() == selectIndex){
+			//print arrow indicating current item
 			print("->");
+			attrset(A_STANDOUT);
+		}
 		else
+			//otherwise print spaces
 			print("  ");
-		print(item->name());
+		//prints up to NAME_SPACING characters
+		printField(item->name(), NAME_SPACING);
 		print("|");
-		print(item->description());
+		printField(item->description(), DESC_SPACING);
 		print("|");
 		waddch(stdscr, item->priority()+48);			//this hack way of getting the priority as a one-string causes this to break on priority levels above 9
 		print("|\n");
+		attroff(A_STANDOUT);
 	}
+	return;
 }
 
 void Menu::display(){
 	std::string msg = "This is a test message to see if I can reliably print stuff out into the screen. Thanks for being patient.\n";
-		 
+	//move cursor to top of window and erase all contents
+	move(0,0);
+	clrtobot();
+	//print a message and the menu
 	print(msg);
 	printMenu();
 }
