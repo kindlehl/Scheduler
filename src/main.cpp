07b986c487bc9170d39a4098c3b30512889063f1
kindlehl <hunter.d.lannon@gmail.com>
2018-04-09 22:58:38 -0700

    catches SIGINT signal to abort safely

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include "../include/menu.h"
 #include <curses.h>
+#include <signal.h>
+#include <functional>
+
 using namespace std;
+
+void sigintHandler(int);
+//points to menu that will be written to file
+
 
 int main(){
 	initscr();	
@@ -10,8 +17,14 @@ int main(){
 	curs_set(0);
 	string home = getenv("HOME");
 	Menu mainMenu(home + "/.schedule");
+	//signal(SIGINT, sigintHandler);
 	while(mainMenu)
 		mainMenu.update();
 	endwin();
 	return 0;
 }
+//when sigint is received, properly exit the menu;
+void sigintHandler(int sig){
+	Menu::run = false;
+}
+
