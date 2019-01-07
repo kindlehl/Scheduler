2a3cd41edce0c69a2bbc6b94ae2d2c58bf042494
Hunter Lannon <hunter.d.lannon@gmail.com>
2019-01-06 23:55:14 -0800

    Made changes to inotify interface
    - switched inotify_init to use non-blocking version
    - switched to watching a directory "$HOME/.scheduler" instead of an
    individual file
    - removed stderr messages that are no longer relevant
    - added base logic for fork-exec-based execution of scripts

--- src/daemon.cpp
+++ src/daemon.cpp
@@ -15,14 +15,12 @@ int main (int argc, char** argv) {
 
 	//child process (daemon) now running
 	
-	inotify_fd = inotify_init(); //initialize inotify subsystem for filesystem events
-
+	inotify_fd = inotify_init1(IN_NONBLOCK); //initialize inotify subsystem for filesystem events
 	if(inotify_fd == -1) {
 		std::cerr << "Could not initialize inotify subsystem" << std::endl;
 		exit(2);
 	}
 
-	std::vector<std::pair<rapidxml::xml_document<>* , char* >> all_configs;
 	std::vector<Profile*> users;
 	std::ifstream etc_passwd("/etc/passwd");
 	std::string token, line;
@@ -40,9 +38,9 @@ int main (int argc, char** argv) {
 		}
 
 		struct stat xml_info;
-		std::string config_path = std::string(temp_array[int(EtcPasswdField::HOME)]) + "/.schedule";
+		std::string config_path = std::string(temp_array[int(EtcPasswdField::HOME)]) + "/.scheduler/.schedule";
 
-		if (temp_array[int(EtcPasswdField::HOME)] != "" && stat(config_path.c_str(), &xml_info) == SUCCESS) {
+		if (temp_array[int(EtcPasswdField::HOME)] != "" && stat(config_path.c_str(), &xml_info) == 0) {
 			std::cerr << "[DEBUG] Successfully loaded file: " << config_path << std::endl;
 			users.push_back(new Profile(temp_array));
 			std::cerr << "[DEBUG] after profile constructor" << std::endl;
@@ -54,52 +52,51 @@ int main (int argc, char** argv) {
 	//do forever
 	while(1) {
 
-		inotify_event event = checkConfigurationFiles(inotify_fd, users);
+		std::vector<inotify_event*> events = checkConfigurationFiles(inotify_fd);
 
-		if (event.wd > 0) {
-			std::cerr << "[DEBUG] FOUND EVENT(S)" << std::endl;
-			Profile* changed_profile = findProfileByDescriptor(event.wd, users);
+		while(!events.empty()) {
+			inotify_event* event = events.back();
+
+			std::cerr << "FIELDS OF EVENT: " << event->wd << " " << event->mask << " " << event->name << std::endl;
+			Profile* changed_profile = findProfileByDescriptor(event->wd, users);
 			if (changed_profile && fileExists(changed_profile->getPath())) {
 				//reload profile
-				changed_profile->clear(); //causes double free :(
+				changed_profile->clear(); //causes double free :( (was actually caused by Profile::Profile(const Profile&))
 				changed_profile->loadConfiguration();
 			}
+			//remove event and free it from vector
+			free(event);
+			events.pop_back();
 		}
 
-
-		std::cerr << "FIELDS OF EVENT: " << event.wd << " " << event.mask << " " << event.name << std::endl;
-		std::cerr << "[DEBUG] FILE MODIFIED" << std::endl;
-		
-
-		//for(auto& u : users) { //for each profile, check events 
-			//returns vector of scripts to execute
-			//std::vector<std::string> scripts = u.handleEvents();
+		for(auto& u : users) { //for each profile, check events 
+			std::vector<std::string> scripts; // = u.handleEvents();
 			
-		int status, pid;
+			int status, pid;
 
-		////if handleEvents returned scripts then run them
-		//if(scripts.size() > 0){
-			//switch(pid) {
-				//case 0: 
-					//execute_scripts(scripts);
-					//break;
-				//case -1:
-					//std::cerr << "Error forking" << std::endl;
-					//break;
-				//default:
-					//child_pids.push_back(pid);
-					//break;
-			//}
-		//}
-
-		//check for dead children
-		for(auto iter = child_pids.begin(); iter != child_pids.end(); iter++) {
-			if (*iter == waitpid(*iter, &status, WNOHANG)) {
-				child_pids.erase(iter);
+			//if handleEvents returned scripts then run them
+			if(scripts.size() > 0){
+				switch(pid) {
+					case 0: 
+						execute_scripts(scripts);
+						break;
+					case -1:
+						std::cerr << "Error forking" << std::endl;
+						break;
+					default:
+						child_pids.push_back(pid);
+						break;
+				}
 			}
-		}
 
-		//}
+			//check for dead children
+			for(auto iter = child_pids.begin(); iter != child_pids.end(); iter++) {
+				if (*iter == waitpid(*iter, &status, WNOHANG)) {
+					child_pids.erase(iter);
+				}
+			}
+
+		}
 	}
 
 	return 0;
